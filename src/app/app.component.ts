91a45b1f5ffd1ee63893910747780045d2cb55e9
tawhidul94 <islamtawhidul910@gmail.com>
2022-08-20 01:05:04 +0600

    nested lopp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  displayVal : string ='';
+  show=false;
+  color="blue";
+  user=["tawhid","sajib","kalam","raz"];
+
+  userDetails=[
+    {name:'Tawhid' ,email:'islam@gmail.com',phone:'018712843'},
+    {name:'sajib' ,email:'islam@gmail.com',phone:'018712843'},
+    {name:'kalam' ,email:'islam@gmail.com',phone:'018712843'},
+    {name:'raz' ,email:'islam@gmail.com',phone:'018712843'}
+
+  ];
+
+  employDetails=[
+    {name:'Tawhid' ,email:'islam@gmail.com',phone:'018712843',accounts :['facebook','tawiter','whatsApp']},
+    {name:'sajib' ,email:'islam@gmail.com',phone:'018712843',accounts :['linked','viber','whatsApp']},
+    {name:'kalam' ,email:'islam@gmail.com',phone:'018712843',accounts :['facebook','tiktalk','whatsApp']},
+    {name:'raz' ,email:'islam@gmail.com',phone:'018712843',accounts :['facebook','youtube','whatsApp']}
+
+  ];
+
+  getData(val:string){
+    console.log(val)
+    this.displayVal= val;
+  }
+
+  count =0;
+  getcount(type :string){
+    type ==='add'? this.count++:this.count--
+  }
+
+ 
 }
